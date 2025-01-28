e454ec9da9373b1e3317d32691d834d37da53334
DownyDowner <bobo.vancauter@live.fr>
2025-01-28 08:24:12 +0100

    feat(queue): Add menu

--- queue.c
+++ queue.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ENQUEUE 1
+#define DEQUEUE 2
+#define SHOW_LIST 3
+#define EXIT 4
+
 typedef struct element
 {
     int number;
@@ -13,3 +18,16 @@ typedef struct queue
     ELEMENT *tail;
     int length;
 } QUEUE;
+
+void printMenu()
+{
+    printf("====================\n");
+    printf("      QUEUE MENU     \n");
+    printf("====================\n");
+    printf("1. Enqueue (Add an element)\n");
+    printf("2. Dequeue (Remove an element)\n");
+    printf("3. Display the queue\n");
+    printf("4. Exit\n");
+    printf("====================\n");
+    printf("Choose an option: ");
+}
