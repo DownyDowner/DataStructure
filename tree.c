061d2f8702550d6ef2b0e98969321dbc647f567c
DownyDowner <bobo.vancauter@live.fr>
2025-02-07 11:52:30 +0100

    Add(tree): added node structure

---
+++ tree.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+} NODE;
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
